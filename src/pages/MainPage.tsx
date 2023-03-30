e8c3739838b1c0220d57ee03399f67bcafbafb8c
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-03-30 14:20:10 +0300

    feat: add hooks at MySearchInput component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,10 @@
 import MyInputWithHooks from '../components/UI/input-search/MyInput-useHooks';
 import Cards from '../components/Cards';
-import MyInput from '../components/UI/input-search/MyInput';
 import React from 'react';
 
 const MainPage = () => {
   return (
     <div>
-      <MyInput query-name="search" type="search" placeholder="Search" />
       <MyInputWithHooks query-name="search2" type="search" placeholder="Search2" />
       <Cards />
     </div>
